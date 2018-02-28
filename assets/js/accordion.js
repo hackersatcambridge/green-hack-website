2f957f559c8fdbc95f583df834a9dd3fcfccb2f1
Charlie Crisp <charliecrisp249@gmail.com>
2018-02-28 00:08:44 +0000

    Add rotating carets

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -7,8 +7,14 @@ for (i = 0; i < acc.length; i++) {
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight){
       panel.style.maxHeight = null;
+      var caret = this.getElementsByTagName("i")[0];
+      console.log(caret);
+      caret.className = "fa fa-angle-right";
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
+      var caret = this.getElementsByTagName("i")[0];
+      console.log(caret);
+      caret.className = "fa fa-angle-down";
     }
   });
 }
